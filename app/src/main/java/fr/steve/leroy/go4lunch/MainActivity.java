d91f4b44b9f71fbd6e07fea32e7e39cfc38cfdf5
steveleroy <casaleroy@gmail.com>
2021-08-17 09:54:34 +0200

    add google_services.json

--- app/src/main/java/fr/steve/leroy/go4lunch/MainActivity.java
+++ app/src/main/java/fr/steve/leroy/go4lunch/MainActivity.java
@@ -1,14 +1,13 @@
 package fr.steve.leroy.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
