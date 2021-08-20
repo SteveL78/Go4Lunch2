6f8155a079297a634a6a8d850038ece4655a3286
steveleroy <casaleroy@gmail.com>
2021-08-20 14:07:52 +0200

    add SignIn layout

--- app/src/main/java/fr/steve/leroy/go4lunch/SignInActivity.java
+++ app/src/main/java/fr/steve/leroy/go4lunch/SignInActivity.java
@@ -3,11 +3,11 @@ package fr.steve.leroy.go4lunch;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin );
     }
 }
