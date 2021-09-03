95fa94554288cbb418f97aa351f11cffc2121174
steveleroy <casaleroy@gmail.com>
2021-09-03 12:43:15 +0200

    add drawer + bottom nav

--- app/src/main/java/fr/steve/leroy/go4lunch/MainActivity.java
+++ app/src/main/java/fr/steve/leroy/go4lunch/MainActivity.java
@@ -1,33 +1,30 @@
 package fr.steve.leroy.go4lunch;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 import fr.steve.leroy.go4lunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding mBinding;
-    FirebaseAuth mAuth;
+    ActivityMainBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
 
-        mBinding = ActivityMainBinding.inflate( getLayoutInflater() );
-        setContentView( mBinding.getRoot() );
+        initView();
 
-        mAuth = FirebaseAuth.getInstance();
 
-        mBinding.logoutButton.setOnClickListener( v -> {
-            FirebaseAuth.getInstance().signOut();
-            startActivity( new Intent( MainActivity.this, SignInActivity.class ) );
-        } );
+
+
+
+    }
+
+    private void initView() {
+        binding = ActivityMainBinding.inflate( getLayoutInflater() );
+        setContentView( binding.getRoot() );
     }
 }
