f5528d844fb9255e915a61a6d58579e03c4b7a7d
steveleroy <casaleroy@gmail.com>
2021-09-04 18:58:08 +0200

    add add fragments

--- app/src/main/java/fr/steve/leroy/go4lunch/MainActivity.java
+++ app/src/main/java/fr/steve/leroy/go4lunch/MainActivity.java
@@ -1,8 +1,13 @@
 package fr.steve.leroy.go4lunch;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import fr.steve.leroy.go4lunch.databinding.ActivityMainBinding;
 
@@ -15,16 +20,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
 
-        initView();
-
-
-
-
-
-    }
-
-    private void initView() {
         binding = ActivityMainBinding.inflate( getLayoutInflater() );
         setContentView( binding.getRoot() );
+
+        // Bottom nav
+        BottomNavigationView bottomNavigationView = findViewById( R.id.activity_main_bottom_navigation );
+        bottomNavigationView.setOnNavigationItemSelectedListener( navListener );        // Setting Map Fragment as main fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace( R.id.activity_main_frame_layout, new MapFragment() )
+                .commit();
+
+
+
     }
+
+
+    // Listener Bottom Nav
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.map_view_item:
+                    selectedFragment = new MapFragment();
+                    break;
+
+                case R.id.list_view_item:
+                    selectedFragment = new ListViewFragment();
+                    break;
+
+                case R.id.workmates_item:
+                    selectedFragment = new WorkmatesFragment();
+                    break;
+            }
+
+            // Begin transaction
+            getSupportFragmentManager().beginTransaction()
+                    .replace( R.id.activity_main_frame_layout, selectedFragment )
+                    .commit();
+            return true;
+        }
+    };
+
+
 }
